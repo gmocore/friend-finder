f90669f0f0b96dc782066f8335b86f9ae913f8f6
Black <gmocore@gmail.com>
2019-09-25 12:48:36 -0600

    added styling

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,17 @@
+var express = require("express");
+var app = express();
+const PORT = 3000;
+
 // GET route for survey.html
 
+app.get("/", (request, response) => {
+  response.send("this is the main page");
+});
+
+app.get("/survey", (request, response) => {
+  response.send("survey page here");
+});
+
 //default catch all that routes to home.html
+
+app.listen(PORT, () => console.log(`App listening on ${PORT}`));
